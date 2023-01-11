eb9ce00bd58315ace4f0e80a56f7b71f40aabb1a
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-11 17:49:25 -0300

    feat: add db name to uri and import UsersModule

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/desafiosharenergy'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
