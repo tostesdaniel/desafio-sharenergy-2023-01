3a2b92706f3752ffa85ee27f4fdd091122c76e48
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-11 12:00:54 -0300

    feat: define user schema

---
+++ app/backend/src/users/schemas/user.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  cpf: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
