f9999d5d8c71672362470f1acd601cc2848bea0c
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-12 18:54:13 -0300

    feat: update auth module
    > import and register JwtModule
    > add JwtStrategy in providers
    > export AuthService
    > register AuthController

--- app/backend/src/auth/auth.module.ts
+++ app/backend/src/auth/auth.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
