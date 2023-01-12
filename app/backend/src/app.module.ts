1a5473eb6cfdc28b9876967112fdc62945dea877
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-12 19:01:36 -0300

    feat: change hardcoded uri to env

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/desafiosharenergy'),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
