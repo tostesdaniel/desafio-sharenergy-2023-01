a0dd63360616807bb2a8e10cf4343b6753d8661d
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-12 08:27:25 -0300

    feat: change email field to username

--- app/frontend/src/routes/Login.tsx
+++ app/frontend/src/routes/Login.tsx
@@ -18,17 +18,17 @@ export default function Login() {
               <form action="#" method="POST" className="space-y-6">
                 <div>
                   <label
-                    htmlFor="email"
+                    htmlFor="username"
                     className="block text-sm font-medium text-gray-700"
                   >
-                    Endereço de email
+                    Nome de usuário
                   </label>
                   <div className="mt-1">
                     <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
+                      id="username"
+                      name="username"
+                      type="text"
+                      autoComplete="username"
                       required
                       className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-yellow-500 focus:outline-none focus:ring-yellow-500 sm:text-sm"
                     />
