38415676da0f50310fb7e091269d6f9d32af248c
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-13 21:01:29 -0300

    feat: create random user request function

--- app/frontend/src/services/requests/index.ts
+++ app/frontend/src/services/requests/index.ts
@@ -2,6 +2,12 @@ import axios from 'axios';
 import Login from '../interfaces/login.interface';
 
 const baseUrl = 'http://localhost:3001/';
+const randomUserUrl = 'https://randomuser.me/api/';
 
 export const requestLogin = (data: Login) =>
   axios.post(`${baseUrl}auth/login`, data);
+
+export const requestRandomUsers = (page: number = 1) =>
+  axios.get(
+    `${randomUserUrl}?page=${page}&results=10&nat=br&seed=desafiosharenergy`
+  );
