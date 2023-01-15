3cabd410d88b350343ecd4d60f99d55ce3605b43
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-15 12:02:18 -0300

    style: add border-radius to menu button

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar({ sidebarOpen, setSidebarOpen }: Props) {
     <div className="sticky top-0 z-10 h-16 bg-gray-100 pl-1 pt-1 shadow sm:p-3 sm:pl-3 md:hidden">
       <button
         type="button"
-        className="inline-flex h-12 w-12 items-center justify-center text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+        className="inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
         onClick={() => setSidebarOpen(true)}
       >
         <span className="sr-only">Abrir menu</span>
