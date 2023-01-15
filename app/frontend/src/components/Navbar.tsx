55992b793a1dccb601f2e3eb9e709a7f7ab7f47c
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-15 10:51:49 -0300

    style: create styles for menu button

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -10,9 +10,13 @@ export default function Navbar({ sidebarOpen, setSidebarOpen }: Props) {
   return (
     <div className="md:pl-64">
       <div className="sticky top-0 z-10 h-16 bg-white shadow">
-        <button type="button" onClick={() => setSidebarOpen(true)}>
+        <button
+          type="button"
+          className="text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
+          onClick={() => setSidebarOpen(true)}
+        >
           <span className="sr-only">Abrir menu</span>
-          <Bars3BottomLeftIcon className="h-6 w-6" />
+          <Bars3BottomLeftIcon className="h-6 w-6" aria-hidden="true" />
         </button>
       </div>
     </div>
