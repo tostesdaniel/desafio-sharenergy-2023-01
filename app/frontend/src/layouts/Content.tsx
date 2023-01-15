3cfc2cc73ca72be0ecbb9eb6def32cb56430abca
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-15 12:40:45 -0300

    feat: update Content component
    Featuring title as props based on location

--- app/frontend/src/layouts/Content.tsx
+++ app/frontend/src/layouts/Content.tsx
@@ -1,5 +1,21 @@
 import { Outlet } from 'react-router-dom';
+import Container from './Container';
 
-export default function Content() {
-  return <Outlet />;
+interface Props {
+  title: string | undefined;
+}
+
+export default function Content({ title }: Props) {
+  return (
+    <main className="flex-1">
+      <div className="py-6">
+        <Container>
+          <h1 className="text-2xl font-semibold text-gray-900">{title}</h1>
+        </Container>
+        <Container>
+          <Outlet />
+        </Container>
+      </div>
+    </main>
+  );
 }
