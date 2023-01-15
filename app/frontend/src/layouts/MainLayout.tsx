5e919a4228eb5042e91a7b9b01036f0b49bf1f04
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-14 22:19:13 -0300

    feat: update layout to use new mobile sidebar

--- app/frontend/src/layouts/MainLayout.tsx
+++ app/frontend/src/layouts/MainLayout.tsx
@@ -1,9 +1,26 @@
+import {
+  ArrowPathIcon,
+  QuestionMarkCircleIcon,
+} from '@heroicons/react/20/solid';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import MobileSidebar from '../components/MobileSidebar';
+
+export const navigation = [
+  { name: 'Usuários aleatórios', href: 'users/random', icon: ArrowPathIcon },
+  { name: 'Gatos HTTP', href: 'http-cat', icon: QuestionMarkCircleIcon },
+];
 
 export default function MainLayout() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
   return (
     <div>
       MainLayout <Outlet />
+      <MobileSidebar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+      />
     </div>
   );
 }
