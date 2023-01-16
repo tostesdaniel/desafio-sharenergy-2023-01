c20d181d5fb30e36366543596b1ec47590c4a48b
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-16 11:46:18 -0300

    feat: remove title from component

--- app/frontend/src/layouts/Content.tsx
+++ app/frontend/src/layouts/Content.tsx
@@ -1,17 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import Container from './Container';
 
-interface Props {
-  title: string | undefined;
-}
-
-export default function Content({ title }: Props) {
+export default function Content() {
   return (
     <main className="flex-1">
       <div className="py-6">
-        <Container>
-          <h1 className="text-2xl font-semibold text-gray-900">{title}</h1>
-        </Container>
         <Container>
           <Outlet />
         </Container>
