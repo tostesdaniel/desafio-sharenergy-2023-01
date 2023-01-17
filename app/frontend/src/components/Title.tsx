b7e4c16d7680fd2aaad6e93f17cfbdf7a41aa29d
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-17 12:02:41 -0300

    style: make title bigger and bolder

--- app/frontend/src/components/Title.tsx
+++ app/frontend/src/components/Title.tsx
@@ -5,7 +5,7 @@ interface Props {
 export default function Title({ title }: Props) {
   return (
     <div className="sm:flex-auto">
-      <h1 className="text-xl font-semibold text-gray-900">{title}</h1>
+      <h1 className="text-2xl font-bold leading-7 text-gray-900">{title}</h1>
     </div>
   );
 }
