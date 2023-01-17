ef7eaa71db67e9823062fa33de8c202a98262d8b
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-17 13:10:57 -0300

    feat: add generate dog button and title

--- app/frontend/src/routes/Dogs.tsx
+++ app/frontend/src/routes/Dogs.tsx
@@ -1,3 +1,17 @@
+import Title from '../components/Title';
+
 export default function Dogs() {
-  return <div>Dogs</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <Title title="Dogs" />
+      <div className="ml-4">
+        <button
+          type="button"
+          className="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          Gerar cão
+        </button>
+      </div>
+    </div>
+  );
 }
