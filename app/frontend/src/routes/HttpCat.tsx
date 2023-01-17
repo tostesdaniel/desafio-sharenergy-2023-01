7653c871b99b89cfe7c338aa32a396c8d62dcc31
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-17 12:37:17 -0300

    feat: create button in header for generating cats
    Displayed only after first cat is generated

--- app/frontend/src/routes/HttpCat.tsx
+++ app/frontend/src/routes/HttpCat.tsx
@@ -5,9 +5,11 @@ import CatSection from '../components/HttpCat/CatSection';
 import Title from '../components/Title';
 import { CatContext } from '../context/CatContext';
 import { httpStatusCodes } from '../services/constants';
+import classNames from '../services/helpers/joinClassnames';
 
 export default function HttpCat() {
-  const [modalOpen, setModalOpen] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [showIntroduction, setShowIntroduction] = useState(true);
   const [image, setImage] = useState(CatCover);
   const [selectedStatusCode, setSelectedStatusCode] = useState(
     httpStatusCodes[0]
@@ -18,11 +20,27 @@ export default function HttpCat() {
     setImage,
     selectedStatusCode,
     setSelectedStatusCode,
+    showIntroduction,
+    setShowIntroduction,
   };
 
   return (
     <>
-      <Title title="Gatos HTTP" />
+      <div className="min-[360px]:flex min-[360px]:items-center min-[360px]:justify-between">
+        <Title title="Gatos HTTP" />
+        <div className="mt-4 min-[360px]:mt-0 min-[360px]:ml-4">
+          <button
+            type="button"
+            onClick={() => setModalOpen(true)}
+            className={classNames(
+              'inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
+              showIntroduction && 'hidden'
+            )}
+          >
+            Gerar gato
+          </button>
+        </div>
+      </div>
       <div className="py-4">
         <CatContext.Provider value={context}>
           <CatSection setModalOpen={setModalOpen} />
