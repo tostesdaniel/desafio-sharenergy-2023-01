28827612e2318222ebb2fe33673b29fdf696ddba
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-16 21:47:03 -0300

    feat: create HTTP Cat Route component

--- app/frontend/src/routes/HttpCat.tsx
+++ app/frontend/src/routes/HttpCat.tsx
@@ -1,3 +1,37 @@
+import { useState } from 'react';
+import CatCover from '../assets/images/cat-cover.jpg';
+import CatGeneratorModal from '../components/HttpCat/CatGeneratorModal';
+import CatSection from '../components/HttpCat/CatSection';
+import Title from '../components/Title';
+import { CatContext } from '../context/CatContext';
+import { httpStatusCodes } from '../services/constants';
+
 export default function HttpCat() {
-  return <div>HttpCat</div>;
+  const [modalOpen, setModalOpen] = useState(true);
+  const [image, setImage] = useState(CatCover);
+  const [selectedStatusCode, setSelectedStatusCode] = useState(
+    httpStatusCodes[0]
+  );
+
+  const context = {
+    image,
+    setImage,
+    selectedStatusCode,
+    setSelectedStatusCode,
+  };
+
+  return (
+    <>
+      <Title title="Gatos HTTP" />
+      <div className="py-4">
+        <CatContext.Provider value={context}>
+          <CatSection setModalOpen={setModalOpen} />
+          <CatGeneratorModal
+            modalOpen={modalOpen}
+            setModalOpen={setModalOpen}
+          />
+        </CatContext.Provider>
+      </div>
+    </>
+  );
 }
