38f2a14d073b9176267fd7dc88e92f9b8995b5d7
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-16 21:25:41 -0300

    feat: create modal to generate cats

---
+++ app/frontend/src/components/HttpCat/CatGeneratorModal.tsx
@@ -0,0 +1,87 @@
+import { Dialog, Transition } from '@headlessui/react';
+import { Dispatch, Fragment, SetStateAction, useContext } from 'react';
+import { CatContext } from '../../context/CatContext';
+import StatusCodeCombobox from './Combobox';
+
+interface Props {
+  modalOpen: boolean;
+  setModalOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function Modal({ modalOpen, setModalOpen }: Props) {
+  const { selectedStatusCode, setImage } = useContext(CatContext);
+
+  return (
+    <Transition show={modalOpen} as={Fragment}>
+      <Dialog as="div" onClose={setModalOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-gray-900"
+                    >
+                      Gere um gato
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Digite ou selecione um status de código de resposta HTTP
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 flex flex-col sm:mt-6">
+                  <StatusCodeCombobox />
+                  <div className="mt-5 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:col-start-2 sm:text-sm"
+                      onClick={() => {
+                        setImage(
+                          `https://http.cat/${selectedStatusCode.status}`
+                        );
+                        setModalOpen(false);
+                      }}
+                    >
+                      Gerar
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:col-start-1 sm:mt-0 sm:text-sm"
+                      onClick={() => setModalOpen(false)}
+                    >
+                      Voltar
+                    </button>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
