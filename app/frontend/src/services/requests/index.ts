bebeb28796cd097698d4f883c54a895bbab99df8
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-17 13:44:20 -0300

    feat: create dog request function

--- app/frontend/src/services/requests/index.ts
+++ app/frontend/src/services/requests/index.ts
@@ -3,9 +3,12 @@ import Login from '../interfaces/login.interface';
 
 const baseUrl = 'http://localhost:3001/';
 const randomUserUrl = 'https://randomuser.me/api/';
+const dogUrl = 'https://random.dog/woof.json';
 
 export const requestLogin = (data: Login) =>
   axios.post(`${baseUrl}auth/login`, data);
 
 export const requestRandomUsers = () =>
   axios.get(`${randomUserUrl}?results=1500&nat=br&seed=desafiosharenergy`);
+
+export const requestDog = () => axios.get(dogUrl);
