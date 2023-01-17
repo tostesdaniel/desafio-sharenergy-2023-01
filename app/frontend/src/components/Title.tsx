05c187aa84b7068b62563c2476c3e6d0773da633
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-17 12:28:51 -0300

    style: make title occupy all space

--- app/frontend/src/components/Title.tsx
+++ app/frontend/src/components/Title.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function Title({ title }: Props) {
   return (
-    <div className="sm:flex-auto">
+    <div className="min-w-0 flex-1">
       <h1 className="text-2xl font-bold leading-7 text-gray-900">{title}</h1>
     </div>
   );
