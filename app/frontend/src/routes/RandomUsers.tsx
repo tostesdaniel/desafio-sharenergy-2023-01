7c6fd11968401221d2917130ea09a3896ec05cc3
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-19 16:37:11 -0300

    refactor: update route to use new table components

--- app/frontend/src/routes/RandomUsers.tsx
+++ app/frontend/src/routes/RandomUsers.tsx
@@ -2,9 +2,10 @@ import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
 import { AxiosError, AxiosResponse } from 'axios';
 import { useCallback, useEffect, useState } from 'react';
 import Notification from '../components/Notification';
-import Pagination from '../components/Pagination';
-import RandomUser from '../components/RandomUser';
+import TableBody from '../components/RandomUsers/TableBody';
+import TableHead from '../components/RandomUsers/TableHead';
 import Searchbar from '../components/Searchbar';
+import Table from '../components/Table';
 import Title from '../components/Title';
 import {
   ApiError,
@@ -12,9 +13,6 @@ import {
   RandomUserResponse
 } from '../services/interfaces/randomUser.interface';
 import { requestRandomUsers } from '../services/requests';
-
-const MAX_RESULTS_PER_PAGE = 10;
-const RESULTS_PER_PAGE = 10;
 
 export interface Error {
   statusText: string | undefined;
@@ -72,57 +70,10 @@ export default function RandomUsers() {
           <Searchbar search={search} setSearch={setSearch} />
         </div>
       </div>
-      <div className="mt-8 flex flex-col">
-        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                    >
-                      Nome
-                    </th>
-                    <th
-                      scope="col"
-                      className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 xl:table-cell"
-                    >
-                      Email
-                    </th>
-                    <th
-                      scope="col"
-                      className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell md:hidden lg:table-cell"
-                    >
-                      Usuário
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Idade
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 bg-white">
-                  {usersToRender
-                    .slice(
-                      (currentPage - 1) * RESULTS_PER_PAGE,
-                      currentPage * MAX_RESULTS_PER_PAGE
-                    )
-                    .map((user) => (
-                      <tr key={user.email}>
-                        <RandomUser user={user} />
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-        <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      </div>
+      <Table currentPage={currentPage} setCurrentPage={setCurrentPage}>
+        <TableHead />
+        <TableBody currentPage={currentPage} usersToRender={usersToRender} />
+      </Table>
       {error && (
         <Notification
           error={{
