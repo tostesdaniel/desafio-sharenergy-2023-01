8c93ea7f55f2aea16a921dc3d2842245b63eab0d
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-20 01:39:10 -0300

    feat: update component to open modal for user creation

--- app/frontend/src/components/Users/CreateUser.tsx
+++ app/frontend/src/components/Users/CreateUser.tsx
@@ -1,17 +1,28 @@
 import { UserPlusIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
+import UserModal from './UserModal';
 
 export default function CreateUser() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
-    <button
-      type="button"
-      className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-    >
-      <div className="mx-auto h-fit w-fit">
-        <UserPlusIcon className="h-12 w-12 text-gray-400" aria-hidden="true" />
-      </div>
-      <span className="mt-2 block text-sm font-medium text-gray-900">
-        Inserir novo usuário
-      </span>
-    </button>
+    <>
+      <button
+        type="button"
+        className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        onClick={() => setModalOpen(true)}
+      >
+        <div className="mx-auto h-fit w-fit">
+          <UserPlusIcon
+            className="h-12 w-12 text-gray-400"
+            aria-hidden="true"
+          />
+        </div>
+        <span className="mt-2 block text-sm font-medium text-gray-900">
+          Inserir novo usuário
+        </span>
+      </button>
+      {modalOpen && <UserModal open={modalOpen} setOpen={setModalOpen} />}
+    </>
   );
 }
