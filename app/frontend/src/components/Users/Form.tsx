7e57be20e162339a1f0a9f226d92e91bbddb0a46
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-20 13:44:32 -0300

    feat: add form validation and password field
    Show a notification when there's an error with the form
    
    TODO: custom error messages for each case

--- app/frontend/src/components/Users/Form.tsx
+++ app/frontend/src/components/Users/Form.tsx
@@ -1,20 +1,44 @@
-import React, { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
+import { ExclamationCircleIcon } from '@heroicons/react/20/solid';
+import React, {
+  ChangeEvent,
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useState
+} from 'react';
+import { z } from 'zod';
+import { validationErrorData } from '../../services/constants';
+import { IUser } from '../../services/interfaces/user.interface';
+import { createUser } from '../../services/requests';
+import Notification, { Error } from '../Notification';
 
 interface Props {
   cancelButtonRef: React.MutableRefObject<null>;
   setOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-const initialState = {
+const initialState: IUser = {
   username: '',
   email: '',
   address: '',
   cpf: '',
   phone: '',
+  password: '',
 };
+
+const userSchema = z.object({
+  username: z.string().min(3).max(255),
+  email: z.string().email().min(3).max(255),
+  address: z.string().min(3).max(255),
+  cpf: z.string().length(11),
+  phone: z.string().length(11),
+  password: z.string().min(3).max(255),
+});
 
 export default function Form({ cancelButtonRef, setOpen }: Props) {
   const [form, setForm] = useState(initialState);
+  const [validationError, setValidationError] = useState(false);
+  const [errorData, setErrorData] = useState<Error>();
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -22,144 +46,177 @@ export default function Form({ cancelButtonRef, setOpen }: Props) {
     setForm({ ...form, [name]: value });
   };
 
+  const submitForm = async (event: FormEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    try {
+      userSchema.parse(form);
+      await createUser(form);
+      setOpen(false);
+    } catch (error) {
+      setErrorData(validationErrorData);
+      setValidationError(true);
+    }
+  };
+
   return (
-    <form className="space-y-8 divide-y divide-gray-200">
-      <div className="space-y-8 divide-y divide-gray-200">
-        <div>
+    <>
+      <form className="space-y-8 divide-y divide-gray-200">
+        <div className="space-y-8 divide-y divide-gray-200">
           <div>
-            <h3 className="text-lg font-medium leading-6 text-gray-900">
-              Cadastro de usuários
-            </h3>
-            <p className="mt-1 text-sm text-gray-500">
-              Preencha todos os campos.
-            </p>
-          </div>
-          <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Nome de usuário
-              </label>
-              <div className="mt-1">
-                <input
-                  type="text"
-                  name="username"
-                  id="username"
-                  value={form.username}
-                  onChange={handleChange}
-                  autoComplete="username"
-                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
-              </div>
+            <div>
+              <h3 className="text-lg font-medium leading-6 text-gray-900">
+                Cadastro de usuários
+              </h3>
+              <p className="mt-1 text-sm text-gray-500">
+                Preencha todos os campos.
+              </p>
             </div>
-
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Endereço de email
-              </label>
-              <div className="mt-1">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  value={form.email}
-                  onChange={handleChange}
-                  autoComplete="email"
-                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
+            <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Nome de usuário
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    value={form.username}
+                    onChange={handleChange}
+                    autoComplete="username"
+                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
               </div>
-            </div>
-
-            <div className="sm:col-span-6">
-              <label
-                htmlFor="address"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Endereço
-              </label>
-              <div className="mt-1">
-                <input
-                  type="text"
-                  name="address"
-                  id="address"
-                  value={form.address}
-                  onChange={handleChange}
-                  autoComplete="street-address"
-                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Senha
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    value={form.password}
+                    onChange={handleChange}
+                    autoComplete="password"
+                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
               </div>
-            </div>
-
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="cpf"
-                className="block text-sm font-medium text-gray-700"
-              >
-                CPF
-              </label>
-              <div className="mt-1">
-                <input
-                  type="text"
-                  name="cpf"
-                  id="cpf"
-                  value={form.cpf}
-                  onChange={handleChange}
-                  autoComplete="on"
-                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Endereço de email
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    value={form.email}
+                    onChange={handleChange}
+                    autoComplete="email"
+                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
               </div>
-            </div>
-
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="phone"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Telefone
-              </label>
-              <div className="mt-1">
-                <input
-                  type="tel"
-                  name="phone"
-                  id="phone"
-                  value={form.phone}
-                  onChange={handleChange}
-                  autoComplete="tel-national"
-                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
+              <div className="sm:col-span-6">
+                <label
+                  htmlFor="address"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Endereço
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="address"
+                    id="address"
+                    value={form.address}
+                    onChange={handleChange}
+                    autoComplete="street-address"
+                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="cpf"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  CPF
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="cpf"
+                    id="cpf"
+                    value={form.cpf}
+                    onChange={handleChange}
+                    autoComplete="on"
+                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="phone"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Telefone
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="tel"
+                    name="phone"
+                    id="phone"
+                    value={form.phone}
+                    onChange={handleChange}
+                    autoComplete="tel-national"
+                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="pt-5">
-        <div className="flex justify-end">
-          <button
-            type="button"
-            className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            onClick={() => setOpen(false)}
-            ref={cancelButtonRef}
-          >
-            Cancelar
-          </button>
-          <button
-            type="submit"
-            className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            onClick={(e) => {
-              e.preventDefault();
-              setOpen(false);
-            }}
-          >
-            Cadastrar
-          </button>
+        <div className="pt-5">
+          <div className="flex justify-end">
+            <button
+              type="button"
+              className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={() => setOpen(false)}
+              ref={cancelButtonRef}
+            >
+              Cancelar
+            </button>
+            <button
+              type="submit"
+              className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={submitForm}
+            >
+              Cadastrar
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+      {validationError && (
+        <Notification
+          error={errorData as Error}
+          icon={{ icon: ExclamationCircleIcon, color: 'text-red-500' }}
+          show={validationError}
+          setShow={setValidationError}
+        />
+      )}
+    </>
   );
 }
