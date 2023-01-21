6afd8b53bf8aadac1127584e462d6c0160c70706
Daniel Tostes <ohtosttnz@gmail.com>
2023-01-20 21:50:14 -0300

    feat: add Context and request refreshing logic
    Extract request to a function
    Create useEffect to watch over refresh state

--- app/frontend/src/routes/Users.tsx
+++ app/frontend/src/routes/Users.tsx
@@ -6,6 +6,7 @@ import Title from '../components/Title';
 import CreateUser from '../components/Users/CreateUser';
 import TableBody from '../components/Users/TableBody';
 import TableHead from '../components/Users/TableHead';
+import { UsersContext } from '../context/UserContext';
 import { IEditUser } from '../services/interfaces/user.interface';
 import { requestUsers } from '../services/requests';
 
@@ -14,18 +15,29 @@ export default function Users() {
   const [usersToRender, setUsersToRender] = useState<IEditUser[]>([]);
   const [search, setSearch] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
+  const [refreshUsers, setRefreshUsers] = useState(false);
 
-  useEffect(() => {
+  const context = { refreshUsers, setRefreshUsers };
+
+  const getAllUsers = async () => {
     requestUsers()
       .then(({ data }: AxiosResponse<IEditUser[]>) => {
         setAllUsers(data);
         setUsersToRender(data);
       })
       .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getAllUsers();
   }, []);
 
+  useEffect(() => {
+    if (refreshUsers) getAllUsers().then(() => setRefreshUsers(false));
+  }, [refreshUsers]);
+
   return (
-    <>
+    <UsersContext.Provider value={context}>
       <div className="sm:flex sm:items-center md:block lg:flex">
         <Title title="Usuários" />
         <div className="mt-4 max-w-sm sm:mt-0 sm:ml-16 sm:flex-none md:ml-0 md:mt-4 lg:mt-0">
@@ -39,6 +51,6 @@ export default function Users() {
           <TableBody currentPage={currentPage} usersToRender={usersToRender} />
         </Table>
       </div>
-    </>
+    </UsersContext.Provider>
   );
 }
